4d700e73a2614d338abce97ebb39e853e3cb9754
ozel-byte <ozelcode@gmail.com>
2021-06-20 21:07:22 -0500

    se agrego el logo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
-
+import Logo from "../src/componentes/logo";
 
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state =  {
-            streamImg: MediaStream
-        }
-    }
-    componentDidMount(){
-        this.camara()
+    
     }
 
-  camara() {
-    navigator.mediaDevices.getUserMedia({audio: false, video: true}).then(stream => {
-        console.log(stream);
-        let camera = document.getElementById("camera");
-        let texto = document.getElementById("texto");
-        texto.style.color = "blue"; 
-        camera.srcObject = stream;
-        this.setState({
-            streamImg: stream
-        })
-    })    
- }
     render(){
        
         return(
             <div>
-                <h1>hel world</h1>
-                <h2 id="texto">funciono</h2>
-                <video id="camera" autoPlay={true}></video>
-                <canvas id="select_canvas" className="select_canvas" width="300px" height="300px"></canvas>
-                <button>Poner en gris</button>
+              <Logo />
             </div>
         )
     }
---
+++ src/componentes/logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Logo extends React.Component{
+    render(){
+        return(
+            <div className="logo">
+                <h2>Tokkio</h2>
+            </div>
+        )
+    }
+}
+
+export default Logo;
