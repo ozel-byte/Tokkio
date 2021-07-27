3c472b39643bf99ecef7c1b157575e500463e37a
ozel-byte <ozelcode@gmail.com>
2021-07-26 22:43:31 -0500

    ass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import  ReactDOM  from "react-dom";
 import App from "./pages/App";
 
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
 ReactDOM.render(
     <App/>,
     document.getElementById("root")
