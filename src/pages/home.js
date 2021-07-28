7508f323894f81a0dd1ff6115f7e4745e4b2df37
KevinOmar07 <Kevinnuricumbo@gmail.com>
2021-07-27 20:12:49 -0500

    cambiado a local host

--- src/pages/home.js
+++ src/pages/home.js
@@ -60,7 +60,7 @@ class Home extends React.Component {
 
    
     getUserData() {
-        axios.get("http://167.172.146.90:3000/user/getUserUsername", {
+        axios.get("http://localhost:3000/user/getUserUsername", {
             params: {
                 username: window.localStorage.getItem('usertokkio')
             }
@@ -78,7 +78,7 @@ class Home extends React.Component {
 
     initSocket() {
         let imagenInvitado = document.getElementsByClassName("home-side-bar-user-invited");
-        const socket = io('http://167.172.146.90:3000');
+        const socket = io('http://localhost:3000');
         this.setState({
             socketIo: socket
         })
